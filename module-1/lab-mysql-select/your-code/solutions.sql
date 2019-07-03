c9c52f5953eb2c32f5049b39e0e44a03295141c5
Messie Tanriverdi <tanriverdi.messie@gmail.com>
2019-07-03 21:30:33 +0200

    week 5 mysql select

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,5 +1,5 @@
 #___________________________________________________
-# CHALLENGE 1
+# CHALLENGE 1 Who Have Published What At Where
 
 /*
 select titaut.au_id as Author_ID, aut.au_lname as Last_Name, aut.au_fname as First_Name, titles.title as Title, pubs.pub_name as Publisher
@@ -23,7 +23,7 @@ where
 */    
     
 select authors.au_id as AUTHOR_ID, authors.au_fname as FIRST_NAME, authors.au_lname as LAST_NAME, titles.title as TITLE, publishers.pub_name as PUBLISHER
-from AUTHORS
+from authors
 
 join titleauthor 
 on authors.au_id = titleauthor.au_id
@@ -37,7 +37,168 @@ on publishers.pub_id = titles.pub_id
 order by authors.au_id;
 
 
+
+
 #___________________________________________________
-# CHALLENGE 2
+# CHALLENGE 2 Who Have Published How Many At Where
+
+# Nombre de titres par publishers
+
+select publishers.pub_name, count(titles.title)
+from publishers, titles
+where publishers.pub_id = titles.pub_id
+group by publishers.pub_name;
 
 
+# Réponse :
+
+select authors.au_id as AUTHOR_ID, authors.au_lname as LAST_NAME, authors.au_fname as FIRST_NAME, publishers.pub_name as PUBLISHER, count(titles.title) as TITLE_COUNT
+
+from authors
+
+join titleauthor 
+on authors.au_id = titleauthor.au_id
+
+join titles 
+on titles.title_id = titleauthor.title_id
+
+join publishers 
+on publishers.pub_id = titles.pub_id
+
+group by authors.au_id, publishers.pub_name
+
+order by greatest(count(titles.title), authors.au_id) desc; 
+
+
+
+
+#___________________________________________________
+# CHALLENGE 3 Best Selling Authors
+
+select authors.au_id as AUTHOR_ID, authors.au_lname as LAST_NAME, authors.au_fname as FIRST_NAME, count(sales.qty) as TOTAL
+
+from AUTHORS
+
+join titleauthor 
+on authors.au_id = titleauthor.au_id
+
+join titles 
+on titles.title_id = titleauthor.title_id
+
+join sales 
+on sales.title_id = titles.title_id
+
+group by authors.au_id
+
+order by count(sales.qty) desc
+
+limit 3;
+
+
+
+
+#___________________________________________________
+# CHALLENGE 4 Best Selling Authors Ranking
+
+
+select authors.au_id as AUTHOR_ID, authors.au_lname as LAST_NAME, authors.au_fname as FIRST_NAME, coalesce(sum(sales.qty), 0) as TOTAL
+
+from AUTHORS
+
+left join titleauthor 
+on authors.au_id = titleauthor.au_id
+
+left join titles 
+on titles.title_id = titleauthor.title_id
+
+left join sales 
+on sales.title_id = titles.title_id
+
+group by authors.au_id
+
+order by sum(sales.qty) desc;
+
+
+
+#___________________________________________________
+# CHALLENGE Bonus Most Profiting Authors
+
+
+
+# bonus avec les advance
+select authors.au_id as AUTHOR_ID, authors.au_lname as LAST_NAME, authors.au_fname as FIRST_NAME, sum(sales.qty), count(titles.advance), sum(distinct titles.advance)
+
+from AUTHORS
+
+left join titleauthor 
+on authors.au_id = titleauthor.au_id
+
+left join titles 
+on titles.title_id = titleauthor.title_id
+
+left join sales 
+on sales.title_id = titles.title_id
+
+
+
+group by authors.au_id
+
+order by greatest(sum(sales.qty), authors.au_id) desc;
+
+# bonus avec les royalty
+select authors.au_fname as FIRST_NAME, sum(sales.qty), titleauthor.royaltyper, titles.royalty, (sum(sales.qty)/100*royalty)/100*royaltyper
+
+from AUTHORS
+
+left join titleauthor 
+on authors.au_id = titleauthor.au_id
+
+left join titles 
+on titles.title_id = titleauthor.title_id
+
+left join sales 
+on sales.title_id = titles.title_id
+
+
+
+group by authors.au_id, titleauthor.royaltyper, titles.royalty
+
+order by greatest(sum(sales.qty), authors.au_id) desc;
+
+
+# bonus avec les royalty
+select authors.au_id as AUTHOR_ID, authors.au_lname as LAST_NAME, authors.au_fname as FIRST_NAME, sum(sales.qty / 100 * royalty / 100 * royaltyper) as PROFIT
+
+from AUTHORS
+
+left join titleauthor 
+on authors.au_id = titleauthor.au_id
+
+left join titles 
+on titles.title_id = titleauthor.title_id
+
+left join sales 
+on sales.title_id = titles.title_id
+
+group by authors.au_id, LAST_NAME, FIRST_NAME
+
+order by authors.au_id desc;
+
+# -> il faut ajouter le prix des livres
+
+select authors.au_id as AUTHOR_ID, authors.au_lname as LAST_NAME, authors.au_fname as FIRST_NAME, sum(sales.qty / 100 * royalty / 100 * royaltyper) as PROFIT, sum(distinct titles.advance) as ADVANCE, sum(sales.qty / 100 * royalty / 100 * royaltyper) + sum(distinct titles.advance) as SOMME
+
+from AUTHORS
+
+left join titleauthor 
+on authors.au_id = titleauthor.au_id
+
+left join titles 
+on titles.title_id = titleauthor.title_id
+
+left join sales 
+on sales.title_id = titles.title_id
+
+group by authors.au_id, LAST_NAME, FIRST_NAME
+
+order by authors.au_id desc;
