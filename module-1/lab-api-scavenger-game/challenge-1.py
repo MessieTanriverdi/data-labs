b344ae65c2a5ef4e9a11fb63c21a81d63d961612
Messie Tanriverdi <tanriverdi.messie@gmail.com>
2019-07-17 21:32:56 +0200

    week 7 mercredi

--- module-1/lab-api-scavenger-game/challenge-1.py
+++ module-1/lab-api-scavenger-game/challenge-1.py
@@ -1,5 +1,18 @@
 # enter your code below
 
+"""
+
 attention, ne pas écrire le token dans le code
 le mettre dans un fichier externe à github
 l'appeler avec un open
+
+
+transformer le json en panda
+au lieu d'utiliser DataFrame
+
+ajouter /forks
+
+
+
+
+"""
